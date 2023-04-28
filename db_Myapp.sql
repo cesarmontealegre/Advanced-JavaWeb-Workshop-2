19bae168594a32eeb40a65c4154b2f904b618bd3
Cesar Fernando Montealegre Correa <cesarmontealegere@gmail.com>
2023-04-27 22:33:53 -0500

    Second Commit

---
+++ db_Myapp.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users_tbl (
+  user_id INT PRIMARY KEY,
+  user_firstname VARCHAR(40) NOT NULL,
+  user_lastname VARCHAR(40) NOT NULL,
+  user_email VARCHAR(60) NOT NULL,
+  user_password VARCHAR(256)
+);
